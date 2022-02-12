0d5c64c5ba25208b31ae380624ca6390092a5553
Chad Powell <chada.powell@outlook.com>
2022-02-12 03:36:15 -0500

    update all files

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
-let alarmSetTime = null;
-let alarmTimeLeft = null;
+let alarmTime = null;
+let alarmTimeout = null;
+const alarmAudio = new Audio("reveille.mp3");
 let alarmTimeValue = document.getElementById("input");
 let weekDays = document.getElementById("days");
 
 let alarmControls = document.getElementById("alarmControl");
 let alarmSet = document.getElementById("alarmSet");
-
-/* CREATE TIME INPUT FOR ALARM */
-let inputAlarm = document.createElement("input");
-inputAlarm.type = "datetime-local";
-alarmSet.appendChild(inputAlarm);
-
-/* CREATE ALARM OFF BUTTON */
-let alarmOffBtn = document.createElement("button");
-alarmOffBtn.id = "alarmOff";
-alarmOffBtn.innerHTML = "Alarm Off";
-alarmControls.appendChild(alarmOffBtn);
-
-/* CREATE SNOOZE BUTTON */
-let snoozeBtn = document.createElement("button");
-snoozeBtn.id = "snooze";
-snoozeBtn.innerHTML = "Snooze";
-alarmControls.appendChild(snoozeBtn);
 
 /* CREATE HEADING */
 let heading = document.getElementById("header");
@@ -29,9 +13,6 @@ let h1 = document.createElement("h1");
 let title = document.createTextNode("JavaScript Clock Project");
 h1.appendChild(title);
 heading.appendChild(title);
-
-const alarmAudio = new Audio("reveille.mp3");
-let alarmTime = null;
 
 /* FUNCTION TO GET TIME AND DISPLAY */
 const digitalClock = function () {
@@ -48,12 +29,8 @@ const digitalClock = function () {
 
   let time = `${hour} : ${minutes} : ${seconds} ${period}`;
 
-  document.getElementById("timeDisplay").innerHTML = time;
+  document.getElementById("timeDisplay").textContent = time;
 };
-
-inputAlarm.addEventListener("input", (e) => {
-  alarmTime = e.target.value;
-});
 
 /* FUNCTION TO GET CURRENT DATE  AND DISPLAY */
 const getDateDisplay = function () {
@@ -80,22 +57,19 @@ const getDateDisplay = function () {
   let currentYear = d.getFullYear();
   let currentFullDate = `${currentDay} ${currentMonth} ${currentDate}, ${currentYear}`;
 
-  document.getElementById("date").innerHTML = currentFullDate;
+  document.getElementById("date").textContent = currentFullDate;
 };
 
 /* FUNCTION TO CONVERT HOUR TO 12HR FORMAT */
 
 const twelveHourFormat = function (hour) {
-  if (hour > 12) {
-    hour = hour - 12;
-  } else {
-    hour = 12;
+  if (hour) {
+    hour % 12 || 12;
   }
   return hour;
 };
 
 /* FUNCTION TO ADD ZERO IF NUM IS LESS THAN 10 */
-
 const addZero = function (i) {
   if (i < 10) {
     i = "0" + i;
@@ -103,25 +77,30 @@ const addZero = function (i) {
   return i;
 };
 
-const setAlarm = function (d, alarmTime) {
-  if (alarmTime) {
-    let currentTime = d;
-    let alarmTimeOut = new Date(alarmTime);
+const setAlarmTime = function (value) {
+  alarmTime = new Date(value).getTime();
+};
 
-    if (alarmTime > currentTime) {
-      alarmTimeOut = alarmTimeOut.getTime() - currentTime.getTime();
-      alarmTimeLeft = setTimeOut(() => alarmAudio.play(), alarmTimeOut);
-    }
+const setAlarm = function () {
+  let currentTime = new Date().getTime();
+  if (alarmTime && alarmTime >= currentTime) {
+    const timeout = alarmTime - currentTime;
+    alarmTimeout = setTimeout(() => alarmAudio.play(), timeout);
   }
 };
 
-/* CREATE SET ALARM BUTTON */
-let setAlarmBtn = document.createElement("button");
-setAlarmBtn.id = "setAlarm";
-setAlarmBtn.innerHTML = "Set Alarm";
-setAlarmBtn.onclick = setAlarm();
-alarmSet.appendChild(setAlarmBtn);
+/* FUNCTION TO TURN ALARM OFF */
+const alarmOff = function () {
+  alarmAudio.pause();
+  if (alarmTimeout) {
+    clearTimeout(alarmTimeout);
+  }
+};
 
+const snooze = function () {
+  setInterval(alarmAudio.pause(), 5000);
+  clearInterval(alarmAudio.play());
+};
 /* FUNCTION TO START PROGRAM & UPDATE CLOCK EVERY SECOND */
 
 const init = function () {
